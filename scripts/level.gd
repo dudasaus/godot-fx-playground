963a67fd1f32f70210dc2448f9917a46aa3c05b3
Austin Dudas <dudasaus@umich.edu>
2024-09-22 11:13:19 -0600

    hurt shader

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 var materials = {
 	"grayscale": load("res://materials/grayscale.tres"),
-	"invert": load("res://materials/invert.tres")
+	"invert": load("res://materials/invert.tres"),
+	"hurt": load("res://materials/hurt.tres")
 }
 
 func _init() -> void:
