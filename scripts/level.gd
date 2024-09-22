9f0a17212eb192ce6d36a912ccc549af47d098dc
Austin Dudas <dudasaus@umich.edu>
2024-09-22 11:51:38 -0600

    pixelate

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 var materials = {
-	"grayscale": load("res://materials/grayscale.tres"),
-	"invert": load("res://materials/invert.tres"),
-	"hurt": load("res://materials/hurt.tres"),
-	"crt-stripes": load("res://materials/crt-stripes.tres"),
+	"Grayscale": load("res://materials/grayscale.tres"),
+	"Invert": load("res://materials/invert.tres"),
+	"Hurt": load("res://materials/hurt.tres"),
+	"CRT stripes": load("res://materials/crt-stripes.tres"),
+	"Pixelate": load("res://materials/pixelate.tres"),
 }
 
-func _init() -> void:
-	print(materials)
+func _ready() -> void:
+		for label in materials.keys():
+			$MenuBar/Effects.add_item(label)
 
 func _on_effects_index_pressed(index: int) -> void:
 	if index == 0:
